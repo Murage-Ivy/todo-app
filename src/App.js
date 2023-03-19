0aad014b91b0faaafcec849403c6b5f66c84d86b
Murage-Ivy <ivymurage2000@gmail.com>
2023-03-19 15:06:04 +0300

    Add routing for login form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from 'react-router-dom';
 import LoginForm from './Components/LoginForm';
 import Sidebar from './Components/Sidebar';
 import RouteList from './Routes';
@@ -6,12 +7,13 @@ import RouteList from './Routes';
 function App() {
   return (
     <>
-      {/* <Sidebar>
-        <RouteList />
-      </Sidebar> */}
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/*" element={<Sidebar>
+          <RouteList />
+        </Sidebar>} />
+      </Routes>
 
-
-      <LoginForm />
     </>
 
 
