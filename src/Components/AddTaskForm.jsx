5f36d15bc81cac8ede4bd5ff68db2c848e2acb3b
Murage-Ivy <ivymurage2000@gmail.com>
2023-03-22 16:27:36 +0300

    Style the task card

--- src/Components/AddTaskForm.jsx
+++ src/Components/AddTaskForm.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function AddTaskForm() {
   return (
-    <div>AddTaskForm</div>
+    <div>AddTaskForm
+      <form>
+        <input type="text" placeholder="Add Task" />
+        <button type="submit">Add</button>
+      </form>
+    </div>
   )
 }
 
