e512d4685b68c7bc067167e832401b80f451d24b
Murage-Ivy <ivymurage2000@gmail.com>
2023-03-23 11:26:07 +0300

    Style add task form

--- src/Components/AddTaskForm.jsx
+++ src/Components/AddTaskForm.jsx
@@ -1,5 +1,8 @@
+import { faCalendar, faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useContext } from 'react'
 import { TaskPageContext } from '../context/TaskPageContext'
+import './Styles/AddTaskForm.css'
 
 function AddTaskForm() {
 
@@ -9,8 +12,11 @@ function AddTaskForm() {
     setTrigger(false)
   }
   return (
-    <div>
-      <h2 className='task-form-title'>Add Task</h2>
+    <div className='task-form'>
+      <div className='task-form-header'>
+        <h2 className='task-form-title'>Add Task</h2>
+        <FontAwesomeIcon icon={faPlus} className='close-icon' />
+      </div>
       <form>
         <div className="task-form-group">
           <label htmlFor="title">Title</label>
@@ -21,28 +27,34 @@ function AddTaskForm() {
           <input type="text" placeholder="Add Description" />
         </div>
 
-        <div className="task-form-group">
+        <div className="task-form-group-date">
+          <div className="start-date">
+            <label htmlFor="date">  Start Date</label>
+            <br />
+            <input type="date" />
+          </div>
 
-          <label htmlFor="date">  Start Date</label>
-          <input type="date" placeholder="Add Date" />
-
-          <label htmlFor="date">  Start Date</label>
-          <input type="date" placeholder="Add Date" />
+          <div className="end-date">
+            <label htmlFor="date">   End Date </label>
+            <br />
+            <input type="date" />
+          </div>
         </div>
         <div className="task-form-group">
           <label htmlFor="status">Status</label>
           <select name="status" id="status">
+            <option selected hidden value="status">Status</option>
             <option value="todo">Todo</option>
             <option value="inprogress">Inprogress</option>
             <option value="complete">Complete</option>
           </select>
         </div>
         <div className="submit-action-btns">
-          <button type="submit" onClick={handleChange}>Cancel</button>
-          <button type="submit">Save</button>
+          <button type="submit" onClick={handleChange} className="add-task-form-btn">Cancel</button>
+          <button type="submit" className="add-task-form-btn">Save</button>
         </div>
-      </form>
-    </div>
+      </form >
+    </div >
   )
 }
 
