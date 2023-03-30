12a3463139dc4ba768f3f28cbe60eb58f64eb549
Murage-Ivy <ivymurage2000@gmail.com>
2023-03-30 11:30:14 +0300

    implement controlled add task form

--- src/Components/AddTaskForm.jsx
+++ src/Components/AddTaskForm.jsx
@@ -14,10 +14,31 @@ function AddTaskForm() {
     status: '',
 
   })
+
+
   const { setTrigger } = useContext(TaskPageContext)
 
   const handleChange = () => {
     setTrigger(false)
+  }
+
+
+  const handleTaskChange = (event) => {
+    const { name, value } = event.target
+    setTask({ ...task, [name]: value })
+  }
+
+  const handleTaskSubmit = (event) => {
+    event.preventDefault();
+    console.log(task)
+
+    setTask({
+      title: '',
+      description: '',
+      start_date: '',
+      end_date: '',
+      status: '',
+    })
   }
   return (
     <div className='task-form'>
@@ -25,32 +46,59 @@ function AddTaskForm() {
         <h2 className='task-form-title'>Add Task</h2>
         <FontAwesomeIcon icon={faPlus} className='close-icon' onClick={handleChange} />
       </div>
-      <form>
+      <form onSubmit={handleTaskSubmit}>
         <div className="task-form-group">
           <label htmlFor="title">Title</label>
-          <input type="text" placeholder="Add Title" />
+          <input
+            type="text"
+            placeholder="Add Title"
+            value={task.title}
+            name="title"
+            onChange={handleTaskChange} />
         </div>
         <div className="task-form-group">
           <label htmlFor="description">Description</label>
-          <textarea cols={100} rows={4} type="text" placeholder="Add Description" />
+          <textarea
+            cols={100}
+            rows={4}
+            type="text"
+            placeholder="Add Description"
+            value={task.description}
+            name="description"
+            onChange={handleTaskChange} />
+
         </div>
 
         <div className="task-form-group-date">
           <div className="start-date">
             <label htmlFor="date">  Start Date</label>
             <br />
-            <input type="date" />
+            <input
+              type="date"
+              value={task.start_date}
+              name='start_date'
+              onChange={handleTaskChange} />
+
           </div>
 
           <div className="end-date">
             <label htmlFor="date">   End Date </label>
             <br />
-            <input type="date" />
+            <input
+              type="date"
+              value={task.end_date}
+              name="end_date"
+              onChange={handleTaskChange} />
+
           </div>
         </div>
         <div className="task-form-group">
           <label htmlFor="status">Status</label>
-          <select name="status" id="status">
+          <select
+            name="status"
+            id="status"
+            value={task.status}
+            onChange={handleTaskChange}>
             <option hidden value="status">Status</option>
             <option value="todo">Todo</option>
             <option value="inprogress">Inprogress</option>
--- src/reducers/taskSlice.jsx
+++ src/reducers/taskSlice.jsx
@@ -1,6 +1,24 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 
-export const  addTask  = () => {
-    
-}
+export const addTask = createAsyncThunk('add/task', async (task) => {
+    const response = await fetch('', {
+        method: "POST",
+        headers: { 'Content-Type': 'Application/json' },
+        body: JSON.stringify({ task })
+    })
+    const data = await response.json()
+
+    if (response.ok) {
+        return data
+    }
+    else {
+        return data.errors
+    }
+
+})
+
+const
+
+const taskSlice 
+
